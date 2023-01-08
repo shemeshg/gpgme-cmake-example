03418c2255aecb1c0c4e4749fd33a84b46f362d1
osx user <gili.shemesh@gmail.com>
2023-01-08 02:27:06 +0200

    commit

--- libpgpfactory/libpgpfactory.h
+++ libpgpfactory/libpgpfactory.h
@@ -8,48 +8,51 @@ public:
   GpgFactory()
   { // Constructor
     std::cout << "Hello" << s;
+    init_gpgme(GPGME_PROTOCOL_OpenPGP);
+    gpgme_error_t err = gpgme_new(&ctx);
+    gpgme_check_version(NULL);
+    if (gpg_err_code(err))
+    {
+      fprintf(stderr, "can not init gpgme: %s\n", gpgme_strerror(err));
+      exit(1);
+    }
   }
 
   ~GpgFactory()
   {
+    gpgme_release(ctx);
     std::cout << "\n Destructor executed";
   }
 
-int listKeys()
-{
-    init_gpgme (GPGME_PROTOCOL_OpenPGP);
-    gpgme_ctx_t ctx;
-    gpgme_key_t key;
-    gpgme_error_t err = gpgme_new(&ctx);
-    gpgme_check_version(NULL);
+  int listKeys()
+  {
 
-    if (!err)
+    gpgme_key_t key;
+    gpgme_error_t err = gpgme_op_keylist_start(ctx, "", 0);
+    while (!err)
     {
-        err = gpgme_op_keylist_start(ctx, "", 0);
-        while (!err)
-        {
-            err = gpgme_op_keylist_next(ctx, &key);
-            if (err)
-                break;
-            printf("%s:", key->subkeys->keyid);
-            if (key->uids && key->uids->name)
-                printf(" %s", key->uids->name);
-            if (key->uids && key->uids->email)
-                printf(" <%s>", key->uids->email);
-            putchar('\n');
-            gpgme_key_release(key);
-        }
-        gpgme_release(ctx);
+      err = gpgme_op_keylist_next(ctx, &key);
+      if (err)
+        break;
+      printf("%s:", key->subkeys->keyid);
+      if (key->uids && key->uids->name)
+        printf(" %s", key->uids->name);
+      if (key->uids && key->uids->email)
+        printf(" <%s>", key->uids->email);
+      putchar('\n');
+      gpgme_key_release(key);
     }
+
     if (gpg_err_code(err) != GPG_ERR_EOF)
     {
-        fprintf(stderr, "can not list keys: %s\n", gpgme_strerror(err));
-        exit(1);
+      fprintf(stderr, "can not list keys: %s\n", gpgme_strerror(err));
+      exit(1);
     }
     return 0;
-}
+  }
 
 private:
+  gpgme_ctx_t ctx;
   std::string s = "GpgFactory";
 
   void init_gpgme(gpgme_protocol_t proto)
