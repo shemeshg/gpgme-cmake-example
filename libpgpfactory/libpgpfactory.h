265a7f0427f5017c05447ab252461d6cb1e626fd
osx user <gili.shemesh@gmail.com>
2023-01-08 20:02:32 +0200

    commit

--- libpgpfactory/libpgpfactory.h
+++ libpgpfactory/libpgpfactory.h
@@ -3,41 +3,89 @@
 #include <vector>
 #include <gpgme.h>
 
-class GpgKeys{  
-  public:
+class GpgKeys
+{
+public:
   bool can_encrypt = false, invalid = false;
-  std::string keyid,name, email;
-  std::string getKeyStr(){
+  std::string keyid, name, email;
+  std::string getKeyStr()
+  {
     return keyid + " # " + name + " <" + email + ">";
   }
 };
 
 class GpgFactory
 {
-public:  
+public:
   GpgFactory() = default;
-  
-  GpgFactory(GpgFactory const&) = delete;
-  GpgFactory& operator =(GpgFactory const&) = delete;
-  GpgFactory(GpgFactory&&) = delete;
-  GpgFactory& operator=(GpgFactory&&) = delete;
 
-  void initPgpFactory(){
-    if (ctxInitialized) {return;}
+  GpgFactory(GpgFactory const &) = delete;
+  GpgFactory &operator=(GpgFactory const &) = delete;
+  GpgFactory(GpgFactory &&) = delete;
+  GpgFactory &operator=(GpgFactory &&) = delete;
+
+  void initPgpFactory()
+  {
+    if (ctxInitialized)
+    {
+      return;
+    }
     init_gpgme(GPGME_PROTOCOL_OpenPGP);
     gpgme_error_t err = gpgme_new(&ctx);
     gpgme_check_version(NULL);
     if (gpg_err_code(err))
     {
-      std::throw_with_nested( std::runtime_error(gpgme_strerror(err)) );
+      std::throw_with_nested(std::runtime_error(gpgme_strerror(err)));
     }
     ctxInitialized = true;
   }
 
   ~GpgFactory()
   {
-    if (!ctxInitialized) {return;}
+    if (!ctxInitialized)
+    {
+      return;
+    }
     gpgme_release(ctx);
+  }
+
+  class GpgMeKeys{
+    public:
+    GpgMeKeys() = default;
+
+    GpgMeKeys(GpgMeKeys const &) = delete;
+    GpgMeKeys &operator=(GpgMeKeys const &) = delete;
+    GpgMeKeys(GpgMeKeys &&) = delete;
+    GpgMeKeys &operator=(GpgMeKeys &&) = delete;
+
+    std::vector<gpgme_key_t> gpgmeKeys = {};
+    ~GpgMeKeys(){
+      for (auto r : gpgmeKeys){
+        gpgme_key_unref(r);
+      }
+    }
+  };
+
+  std::unique_ptr<GpgMeKeys> getGpgMeKeys(std::vector<std::string> patterns)
+  {
+    checkCtxInitialized();    
+    std::unique_ptr<GpgMeKeys> gmk = std::make_unique<GpgMeKeys>();
+     
+
+    for (auto r : patterns)
+    {
+      gpgme_key_t key = nullptr;
+      gpgme_error_t err = gpgme_get_key (ctx,r.c_str(),&key,1);
+      if (gpg_err_code(err))
+      {
+        std::throw_with_nested(std::runtime_error(r + ": " + gpgme_strerror(err)));
+      } else {
+        gmk->gpgmeKeys.push_back(key);
+      }     
+      
+    }
+    gmk->gpgmeKeys.push_back(NULL);
+    return gmk;
   }
 
   std::vector<GpgKeys> listKeys(const std::string pattern = "")
@@ -53,47 +101,53 @@ public:
       if (err)
         break;
       GpgKeys k;
-      k.can_encrypt =  key->subkeys->can_encrypt;
+      k.can_encrypt = key->subkeys->can_encrypt;
       k.keyid = key->subkeys->keyid;
       k.name = key->uids->name;
       k.email = key->uids->email;
       k.invalid = key->uids->invalid;
       retKeys.push_back(std::move(k));
       k.invalid = key->uids->invalid;
-      gpgme_key_release(key);
+      gpgme_key_unref(key);
     }
 
     if (gpg_err_code(err) != GPG_ERR_EOF)
     {
-      std::throw_with_nested( std::runtime_error(gpgme_strerror(err)) );
+      std::throw_with_nested(std::runtime_error(gpgme_strerror(err)));
     }
     return retKeys;
   }
 
-  void setArmor(bool t){
+  void setArmor(bool t)
+  {
     checkCtxInitialized();
-    if (t) {
-      gpgme_set_armor (ctx,1);
+    if (t)
+    {
+      gpgme_set_armor(ctx, 1);
       isArmor = true;
-    } else {
-      gpgme_set_armor (ctx,0);
+    }
+    else
+    {
+      gpgme_set_armor(ctx, 0);
       isArmor = false;
     }
   }
+
 private:
-  
-  gpgme_ctx_t ctx=nullptr;
+  gpgme_ctx_t ctx = nullptr;
   bool ctxInitialized = false;
-  bool isArmor = false; //Produce output in text mode
-  void checkCtxInitialized(){
-    if (!ctxInitialized) {
-      std::throw_with_nested( std::runtime_error("checkCtxInitialized") );
+  bool isArmor = false; // Produce output in text mode
+  void checkCtxInitialized()
+  {
+    if (!ctxInitialized)
+    {
+      std::throw_with_nested(std::runtime_error("checkCtxInitialized"));
     }
   }
-  
+
   void init_gpgme(gpgme_protocol_t proto)
   {
-    gpgme_error_t err=0;
+    gpgme_error_t err = 0;
 
     gpgme_check_version(NULL);
     setlocale(LC_ALL, "");
@@ -105,7 +159,7 @@ private:
     err = gpgme_engine_check_version(proto);
     if (err)
     {
-      std::throw_with_nested( std::runtime_error(gpgme_strerror(err)) );      
+      std::throw_with_nested(std::runtime_error(gpgme_strerror(err)));
     }
   }
 };
--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@ int main(int, char **)
 {
     GpgFactory g{};
     g.initPgpFactory();
+    g.setArmor(true);
     for(auto r : g.listKeys()){
         std::cout<<"we have "<<r.getKeyStr()<<"\n";
     }
-
+    g.getGpgMeKeys({"shemeshg",""});
     
 }
