248a0b943f22fc41f598513250e880ea19a4602f
osx user <gili.shemesh@gmail.com>
2023-01-09 21:09:53 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -9,14 +9,15 @@ int main(int, char **)
 {
     GpgFactory g{};
     g.initPgpFactory();
-    //g.setArmor(true);
+    g.setArmor(true);
     //g.setTextmode(true);
     for(auto r : g.listKeys()){
         std::cout<<"we have "<<r.getKeyStr()<<"\n";
     }
     //g.getGpgMeKeys({"shemeshg"});
     g.setCtxSigners({"shemeshg"});
-    //enum{TO_STD_STRING, TO_CONSOLE, TO_FILE} 
+    //setupToEmpty() setupToFile()  #to_class, setupFromString("string"), setupFromFile() # from_class
+    //exportString(), exportConsole(), exportFile() 
     //encrypt without sign
     //gpgme_data_new_from_file
     std::string out = g.encryptSign("shalom olam",{"shemeshg"});
