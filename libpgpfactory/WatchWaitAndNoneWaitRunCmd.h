96e517a42a469e544d00347a3620fa4c71c62f8e
osx user <gili.shemesh@gmail.com>
2023-01-24 23:04:58 +0200

    implemented mutex

--- libpgpfactory/WatchWaitAndNoneWaitRunCmd.h
+++ libpgpfactory/WatchWaitAndNoneWaitRunCmd.h
@@ -6,8 +6,11 @@ public:
   std::vector<WatchWaitAndNoneWaitRunCmdItem> waitItems{};
   std::vector<WatchWaitAndNoneWaitRunCmdItem> noneWaitItems{};
 
+  std::mutex g_pages_mutex;
+
   WatchWaitAndNoneWaitRunCmdItem &addWithWait(std::string uniqueId, std::string fileName, std::string tmpFolder)
   {
+    std::lock_guard<std::mutex> guard(g_pages_mutex);
     WatchWaitAndNoneWaitRunCmdItem i{uniqueId, fileName, tmpFolder};
     waitItems.push_back(i);
     return waitItems.back();
@@ -16,6 +19,7 @@ public:
   void runWithWait(WatchWaitAndNoneWaitRunCmdItem &i)
   {
     i.runWithWait();
+    std::lock_guard<std::mutex> guard(g_pages_mutex);
     waitItems.erase(std::remove_if(waitItems.begin(),
                                    waitItems.end(),
                                    [&](const WatchWaitAndNoneWaitRunCmdItem itm) -> bool
@@ -25,6 +29,7 @@ public:
 
   WatchWaitAndNoneWaitRunCmdItem &addWithOutWait(std::string uniqueId, std::string fileName, std::string tmpFolder)
   {
+    std::lock_guard<std::mutex> guard(g_pages_mutex);
     WatchWaitAndNoneWaitRunCmdItem i{uniqueId, fileName, tmpFolder};
     noneWaitItems.push_back(i);
     noneWaitItems.back().runWithoutWait();
@@ -33,6 +38,7 @@ public:
 
   void closeWithoutWaitItem(std::string uniqueId)
   {
+    std::lock_guard<std::mutex> guard(g_pages_mutex);
     noneWaitItems.erase(std::remove_if(noneWaitItems.begin(),
                                        noneWaitItems.end(),
                                        [&](const WatchWaitAndNoneWaitRunCmdItem itm) -> bool
