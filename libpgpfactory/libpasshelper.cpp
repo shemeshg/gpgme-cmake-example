0beee8959a77e2c3f4a8a35adcd39d68c7bb78aa
shemeshg <gili.shemesh@gmail.com>
2023-04-17 03:21:16 +0300

    commit

--- libpgpfactory/libpasshelper.cpp
+++ libpgpfactory/libpasshelper.cpp
@@ -5,13 +5,19 @@
 
 void PassHelper::reEncryptFile(std::string pathFileToReEncrypt, std::vector<std::string> encryptTo)
 {
-    std::string backupFile = pathFileToReEncrypt + "backup";
-    std::filesystem::rename(pathFileToReEncrypt,backupFile);
-    PgpmeDataRII ein{backupFile, FROM_FILENAME},
-    emem{},
-    eout{pathFileToReEncrypt, TO_FILENAME};
-    decryptValidate(ein, emem, false);
-    emem.getString(); //get fseek to end of buffer
-    encryptSign(emem, eout, encryptTo, true);
-    std::filesystem::remove(backupFile);
+    try {
+        std::string backupFile = pathFileToReEncrypt + "backup";
+        std::filesystem::rename(pathFileToReEncrypt,backupFile);
+        PgpmeDataRII ein{backupFile, FROM_FILENAME},
+        emem{},
+        eout{pathFileToReEncrypt, TO_FILENAME};
+        decryptValidate(ein, emem, false);
+        emem.getString(); //get fseek to end of buffer
+        encryptSign(emem, eout, encryptTo, true);
+        std::filesystem::remove(backupFile);
+    }
+    catch (...){
+        std::filesystem::rename(pathFileToReEncrypt + "backup", pathFileToReEncrypt);
+        throw;
+    }
 }
