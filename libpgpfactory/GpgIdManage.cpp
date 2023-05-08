8b83ecc8f4613d8042ada2a5b81b059a3a39eb76
shemeshg <gili.shemesh@gmail.com>
2023-05-08 13:04:08 +0300

    commit

--- libpgpfactory/GpgIdManage.cpp
+++ libpgpfactory/GpgIdManage.cpp
@@ -1,26 +1,6 @@
 #include "GpgIdManage.h"
 #include <iterator>
 
-// trim from start (in place)
-static inline void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-                return !std::isspace(ch);
-            }));
-}
-
-// trim from end (in place)
-static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-                return !std::isspace(ch);
-            }).base(), s.end());
-}
-
-// trim from both ends (in place)
-static inline void trim(std::string &s) {
-    rtrim(s);
-    ltrim(s);
-}
-
 void GpgIdManage::init(std::string _currentPath, std::string _stopPath, PassHelper *_ph)
 {
     currentPath = _currentPath;
