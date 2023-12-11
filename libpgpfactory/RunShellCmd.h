c1d578312057443ea072e82df5cc9e988156ab39
shemeshg <gili.shemesh@gmail.com>
2023-12-11 15:09:01 +0200

    commit windowsX86

--- libpgpfactory/RunShellCmd.h
+++ libpgpfactory/RunShellCmd.h
@@ -70,7 +70,11 @@ private:
      {
       std::array<char, 128> buffer;
       std::string result;
-      std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+      #ifdef _WIN32
+        std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
+      #else
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+      #endif  
       if (!pipe)
       {
         std::throw_with_nested( std::runtime_error("popen() failed!"));
--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 
-#include <pybind11/embed.h> 
-namespace py = pybind11;
-
 #include "libpasshelper.h"
 #include "GpgIdManage.h"
 #include "RunShellCmd.h"
@@ -19,24 +16,12 @@ public:
     }
 };
 
-PYBIND11_EMBEDDED_MODULE(pass_simple, m) {
-    py::class_<PassSimpleBal>(m, "PassSimpleBal")        
-        .def(py::init<>())
-        .def("listKeys", &PassSimpleBal::listKeys,"list keys",
-             py::arg("pattern") = "", py::arg("secret_only") = false);            
-}
+
 
 int main(int, char **)
 {   
+    PassSimpleBal p{};
+    p.listKeys();
     
-    py::scoped_interpreter guard{}; //start pybind11
-    py::exec(R"(
-        import pass_simple
-        c=pass_simple.PassSimpleBal()
-        c.listKeys()
-    )");
-    for (std::string line; std::getline(std::cin, line);) {
-        py::exec(line);
-    }
-
+   return 0;
 }
